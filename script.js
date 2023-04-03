413e16599e7d9fa353a767846a0af522a7b19a4c
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-04-03 19:53:19 -0300

    home finalizada

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ $(document).ready(function() {
         }
     });
 })
+
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
