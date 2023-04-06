760c423ffc944bfc4868ce44a169a2205e004181
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-04-06 15:40:30 -0300

    imagens, hover, cores alterados

--- script2.js
+++ script2.js
@@ -6,26 +6,11 @@ window.addEventListener('scroll', function() {
     }
   });
 
-document.querySelector('.menu-btn').addEventListener('click', function() {
+  document.querySelector('.menu-btn').addEventListener('click', function() {
     document.querySelector('.navbar .menu').classList.toggle('active');
-    document.querySelector('.menu-btn i').classList.toggle('active');
   });
 
 document.querySelector('.menu').addEventListener('click', function() {
     document.querySelector('.navbar .menu').classList.remove('active');
 })
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     new TypeIt(".animated", {
-//         speed: 300,
-//         strings: "Yan Pedro"
-//     }).go()
-// })
-
-document.querySelector('.menu').addEventListener('click', function() {
-    window.scrollTo({
-      scrollTo: document.querySelector('.skills'),
-      behavior: 'smooth'
-    });
-  });
 
