7f6dbba30d480988f1faf6063b8a7ccf3053ce70
Brenner Paiva <114958953+brennerpaiva@users.noreply.github.com>
2023-05-20 11:34:50 -0300

    adicionado animação da imagem da home

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        if(this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-    });
-})
+window.addEventListener('scroll', function() {
+    if (this.scrollY > 20) {
+      document.querySelector('.navbar').classList.add('sticky');
+    } else {
+      document.querySelector('.navbar').classList.remove('sticky');
+    }
+  });
 
-$('.menu-btn').click(function(){
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
-});
+  document.querySelector('.menu-btn').addEventListener('click', function() {
+    document.querySelector('.navbar .menu').classList.toggle('active');
+  });
+
+
+const video = document.querySelectorAll('.projeto-video');
+        for (let i = 0; i < video.length; i++) {
+            video[i].addEventListener('mouseenter',
+            function(e) {
+                video[i].play()
+            })
+            video[i].addEventListener('mouseout',
+            function(e) {
+                video[i].pause()
+            })
+}
+
+ function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 250 * i);
+    });
+  }
+
+ const elemento = document.querySelector(".text-2");
+
+
+
+typeWriter(elemento);
+
